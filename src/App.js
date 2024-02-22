2f22f499e9c2444efe5638537b514a9521cfffc7
Hongaproject <tkwlscjq99@gmail.com>
2024-02-22 15:17:23 +0900

    라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
 
+  // 한국관광공사에서 제공하는 TourAPI를 사용하고
+  // 디자인은 tailwind CSS를 사용하여 시간 단축을 하려고 합니다.
+  // 로그인, 댓글, 좋아요 기능을 추가하기 위해 firebase를 사용합니다.
+
+  // 우선 먼저 제작 후 그 뒤에 상태관리등을 보수하려고 합니다.
+
+  // 라이브러리 사용 
+  // react-router-dom
+  // axios
+  // tailwind CSS
+  // firebase
+
   return (
     <div>
       
