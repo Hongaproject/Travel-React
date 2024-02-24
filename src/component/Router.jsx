3a1fff9c2f5adaf07997b910951330998e9246c9
Hongaproject <tkwlscjq99@gmail.com>
2024-02-24 11:11:41 +0900

    기본틀 제작 중

--- src/component/Router.jsx
+++ src/component/Router.jsx
@@ -1,14 +1,14 @@
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
-import Nav from "../layout/Nav";
 
 function Router() {
 
     return (
       <>
         <Header />
-        <Nav />
-        
+          <div className="h-screen w-11/12 m-auto mt-6 pt-6">
+
+          </div>
         <Footer />
       </>
     );
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,186 @@
+import classNames from "classnames";
+import { useState } from "react";
+
 function Header() {
 
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [menuToggle1, setMenuToggle1] = useState(false);
+
     return (
-      <div className="border-b-4 border-indigo-500">
-        <h1>대한민국 이곳저곳</h1>
-      </div>
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+              <span class="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">대한민국 이곳저곳</span>
+          </a>
+          <div class="flex md:order-2">
+            <button>
+              <div class="relative hidden md:block">
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                  </svg>
+                  <span class="sr-only">Search icon</span>
+              </div>
+            </button>
+            <div class="flex items-center space-x-6 rtl:space-x-reverse ml-4">
+              <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+            </div>
+          </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+              <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                  <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">여행지</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">축제</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">공연/행사</a>
+                </li>
+              </ul>
+            </div>
+            {/* mobile menu */}
+            <div className="md:hidden flex">
+              <button onClick={() => setMenuToggle1(!menuToggle1)}>
+                {menuToggle1 ? (
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                  </svg>
+                ) : (
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                  </svg>
+                )}
+              </button>
+            </div>
+
+            <div className="md:hidden flex ">
+              <button onClick={() => setMenuToggle(!menuToggle)}>
+                {menuToggle ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                  
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </nav>
     );
+
+  return (
+    <nav className="bg-gray-100">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          {/* 메뉴 */}
+          <div className="flex space-x-4">
+            <div>
+              <a href="#" className="flex items-center py-5 px-2 text-gray-700">
+                <span className="text-3xl font-semibold whitespace-nowrap">대한민국 이곳저곳</span>
+              </a>
+            </div>
+          </div>
+
+          {/* 메뉴2 */}
+          <div className="hidden md:flex items-center space-x-1">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+            <a href="#" className="py-2 px-3">
+              Login
+            </a>
+          </div>
+
+        </div>
+          {/* mobile menu */}
+          <div className="md:hidden flex ">
+            <button onClick={() => setMenuToggle(!menuToggle)}>
+              {menuToggle ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+                
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          <div className="md:hidden flex">
+          <button onClick={() => setMenuToggle1(!menuToggle1)}>
+              {menuToggle1 ? (
+                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                 </svg>
+              ) : (
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                 </svg>
+              )}
+            </button>
+          </div>
+      </div>
+
+      {/* mobile menu items */}
+      <div className={classNames("md:hidden", { hidden: !menuToggle })}>
+        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+          Pricing
+        </a>
+        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+          Features
+        </a>
+      </div>
+    </nav>
+  );
   }
   
   export default Header;
